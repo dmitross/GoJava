f5b8a6b0890bbc6c02531ebe49ede4b11ba3398e
Dmitros <dmitros632@gmail.com>
2016-01-11 14:12:59 +0200

    Mod6 - Exeptons

---
+++ Mod6/File/Audio.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Audio extends File {
+
+    private double size;
+    private boolean play;
+
+    public void Type() {
+        System.out.println("Type of file is 'Audio'");
+        System.out.println("Please, enter the size of song:");
+
+        final Scanner scan = new Scanner(System.in);
+        double size = scan.nextDouble();
+
+        try {
+            size = Double.parseDouble(String.valueOf(size));
+            System.out.println("Size is: " + size + " Mb");
+            System.out.println();
+
+            if (size < 0) {
+                throw new NegativeSizeException(size);
+            }
+
+            if (play) {
+                throw new IllegalStateException("Already played");
+            }
+        } catch (NegativeSizeException exNegativeSize) {
+            exNegativeSize.printStackTrace();
+        }
+    }
+}
