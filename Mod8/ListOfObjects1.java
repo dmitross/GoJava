fa493721fbfb9a4be9691ed97510c79ee5fbfa6f
dmitross <dmitros632@gmail.com>
2016-01-21 17:15:27 +0000

    Mod8

---
+++ Mod8/ListOfObjects1.java
@@ -0,0 +1,22 @@
+package Mod8.Exs2;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ListOfObjects1 {
+
+
+
+    public static void main(String[] args) {
+
+
+        SortedSet<String> list = new TreeSet<String>();
+
+        list.add("Piano");
+        list.add("Trump");
+        list.add("Guitar");
+
+        System.out.println(list);
+
+    }
+}
